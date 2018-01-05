57990c52545addbd10138242be8c7649a9c0f4b7
Rolando Pereira <rolando_pereira@sapo.pt>
2018-01-05 23:00:12 +0000

    Setup code to trigger issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TreeNode } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    // Create a tree with the following shape:
+    //
+    //   A
+    //   +--> B
+    //   +--> C
+  nodes: TreeNode[] = [
+    {
+      label: 'A',
+      data: 'A',
+      expanded: true,
+      children: [
+        { label: 'B', data: 'B' },
+        { label: 'C', data: 'C' },
+      ]
+    }
+  ];
+
+  selectedValues: TreeNode[] = [];
+
+  selectEntry(): void {
+    // Add the "C" tree node to the selection
+    const node = this.nodes[0].children[1];
+    this.selectedValues = this.selectedValues.concat(node);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/primeng';
+import { TreeModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
